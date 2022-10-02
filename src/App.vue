265c28a4026c0ad7e73d7a5c5f3fbd6f9caa301f
Yasuyuki Saito <ya_saito@uluru.jp>
2022-10-02 21:42:07 +0900

    テストデータ追加

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,63 @@ import { ref } from 'vue'
 import TiptapEditor from './components/TiptapEditor.vue'
 import HtmlPreviewPanel from './components/HtmlPreviewPanel.vue'
 
+/** サンプルHTML */
+const sampleHtml = `<h1>h1</h1>
+<h2>h2</h2>
+<h3>h3</h3>
+<h4>h4</h4>
+<h5>h5</h5>
+<p>text</p>
+<p><strong>太字</strong></p>
+<p><em>斜体</em></p>
+<p><s>取り消し</s></p>
+<p><u>下線</u></p>
+<p><mark data-color="#ffcc00" style="background-color: #ffcc00">マーカー</mark></p>
+<p><span style="color: #0000ff">色付き</span></p>
+改行<br><br>
+<p style="text-align: center">中央寄せ</p>
+<p style="text-align: right">右寄せ</p>
+<hr>
+<p></p>
+<ul>
+  <li><p>リスト</p>
+    <ul>
+      <li><p>リストネスト１</p>
+        <ul>
+          <li><p>リストネスト２</p>
+            <ul><li><p>リストネスト３</p></li></ul>
+          </li>
+          <li><p>リストネスト２</p></li>
+        </ul>
+      </li>
+    </ul>
+  </li>
+</ul>
+<p></p>
+<ol>
+  <li><p>数字リスト</p>
+    <ol>
+      <li><p>リストネスト１</p>
+        <ol>
+          <li><p>リストネスト２</p>
+            <ol>
+              <li><p>リストネスト３</p></li>
+              <li><p>リストネスト３</p></li>
+              <li><p>リストネスト３</p></li>
+            </ol>
+          </li>
+          <li><p>リストネスト２</p></li>
+        </ol>
+      </li>
+    </ol>
+  </li>
+</ol>
+<br>
+<p><a target="_blank" rel="noopener noreferrer nofollow" href="https://www.google.com/">URLリンク</a></p>
+`
+
 /** HTMLコンテンツ */
-const content = ref('<h1>h1</h1><h2>h2</h2><h3>h3</h3><h4>h4</h4>')
+const content = ref(sampleHtml)
 </script>
 
 <template>
